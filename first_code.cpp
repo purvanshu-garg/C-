90acf8eaccc08c7f5a9b3b4be63a0926a3037cc7
purvanshu-garg <purvanshugarg0702@gmail.com>
2025-10-01 17:05:12 +0530

    Compiling and running

--- first_code.cpp
+++ first_code.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 //you could also use using namespace std; and then no need to use std::cout , just use cout 
+// to run a command first you need to compile using g++ Program-name -o alias
+// then in order to run use .\alias
 int main(void){
     std::cout << "hello\n";
     return 0;
